a50b5215f4e49b048377ffbbe99fc9f3c30ceaae
jihyun <372491@naver.com>
2023-03-26 19:00:26 +0900

    의존성 주입 후 @Service, @Repository 어노테이션 추가

---
+++ src/main/java/com/example/webstudy/sample/SampleDAO.java
@@ -0,0 +1,7 @@
+package com.example.webstudy.sample;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SampleDAO {
+}
---
+++ src/main/java/com/example/webstudy/sample/SampleService.java
@@ -0,0 +1,13 @@
+package com.example.webstudy.sample;
+
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@ToString
+@Service
+public class SampleService {
+
+    @Autowired
+    private SampleDAO sampleDAO;
+}
