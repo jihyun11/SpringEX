d0d09e85ea72112d8ef2398d2c76c07d4e22f630
jihyun <372491@naver.com>
2023-03-28 17:26:39 +0900

    스프링과 mybatis 연동

---
+++ src/main/java/com/example/webstudy/mapper/TimeMapper2.java
@@ -0,0 +1,6 @@
+package com.example.webstudy.mapper;
+
+public interface TimeMapper2 {
+
+    String getNow();
+}
--- src/test/java/com/example/webstudy/sample/SampleTests.java
+++ src/test/java/com/example/webstudy/sample/SampleTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
